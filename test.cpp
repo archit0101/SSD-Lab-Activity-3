27b0546f12d075c46affd65a3faafb791e30718d
Naman jain <namanjain14310@gmail.com>
2020-10-02 00:13:28 +0530

    test.cpp added

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std; 
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define deb(x) cout<<#x<<" "<<x<<endl
+#define in(x) cin>>x
+#define fo(i,n) for(int i=0; i<n; i++)
+#define endl "\n"
+#define all(v) v.begin(),v.end()
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+ 
+int main() 
+{   stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    cout<<endl<<"stack :";
+            while(!s.empty()){
+                cout<<s.top()<<" ";
+                s.pop();
+            }
+    return 0;
+}
